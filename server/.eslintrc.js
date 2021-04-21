685080e43680991f2a6da94d5c4d447ed29a1d1c
Luis Hankel <kaes3kuch3n@gmail.com>
2021-04-21 11:51:54 +0200

    Switched backend to typescript (#2)

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
