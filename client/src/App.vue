7cf2619a5efd960418aacfffa827201c059eb647
Robin <robin.decker@student.htw-berlin.de>
2021-04-21 12:15:24 +0200

    #6 added bootstrap 5, added imimap logo and header background

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <header>
-    <h1>IMIMAP2</h1>
+  <header class="container-fluid imimap-header">
+    <div class="row">
+      <div class="col-2">
+        <img src="/assets/logo.png" alt="">
+      </div>
+      <div class="col-10">NAV</div>
+    </div>
   </header>
   <main>
+    <div class="dropdown">
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton1"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        Check Bootstrap
+      </button>
+      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        <li><a class="dropdown-item" href="#">Action</a></li>
+        <li><a class="dropdown-item" href="#">Another action</a></li>
+        <li><a class="dropdown-item" href="#">Something else here</a></li>
+      </ul>
+    </div>
     <router-view></router-view>
   </main>
   <footer></footer>
@@ -19,4 +40,15 @@ export default {
 </script>
 
 <style lang="scss">
+.imimap-header {
+  background: url('/assets/topbar-background.png'),#292929;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  width: 100%;
+  border-radius: 0 !important;
+  height: 129px;
+  text-align: center;
+  overflow: visible;
+  margin-bottom: 20px;
+}
 </style>
