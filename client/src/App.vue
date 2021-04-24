4149bfa92963fb1655f28d64e17961f333649b45
Robin <robin.decker@student.htw-berlin.de>
2021-04-24 15:33:39 +0200

    #6 added font awesome icons, header markup

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,11 +9,49 @@
       <div class="col-10">
         <div class="container">
           <div class="row">
-            <div class="col-12"></div>
+            <div class="col-12">
+              <ul class="nav float-right imi-nav-right">
+
+                <li class="imimap-nav-right-li">
+                  <a href="/de/help">
+                    <font-awesome-icon icon="question-circle" />
+                  </a>
+                </li>
+                <li> &nbsp;</li>
+                <li class="imimap-nav-right-li">
+                  <a href="/de/students/778">
+                    <font-awesome-icon icon="user" />
+                  </a>
+                </li>
+
+                <li class="dropdown imimap-nav-right-li">
+                  <a href="#" id="drop3" role="button"
+                     class="dropdown-toggle" data-bs-toggle="dropdown"
+                  >
+                    <font-awesome-icon icon="cog" />
+                  </a>
+                  <ul class="dropdown-menu" role="menu" aria-labelledby="drop3">
+                    <li class="locale-de dropdown-item">
+                      <a href="/de">de</a>
+                    </li>
+                    <li class="locale-en dropdown-item">
+                      <a href="/en">en</a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="imimap-nav-right-li">
+                  <a rel="nofollow" data-method="delete" href="/de/users/sign_out">
+                    <font-awesome-icon icon="sign-out-alt" />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <div class="row">
             <div class="col-12">
-              <nav class="mt-4 ml-3 pb-3 navbar navbar-expand-md navbar-dark">
+              <nav class="mt-4 ml-3 pb-3 navbar navbar-expand-md navbar-dark bg-dark"
+                   id="imi-maps-navbar-main"
+              >
                 <button class="navbar-toggler imi-map-toggler"
                         type="button" data-toggle="collapse"
                         data-target="#imi-map-navbar-core"
@@ -84,6 +122,35 @@ export default {
 
 <style lang="scss">
 $screen-md: 768px;
+html {
+  font-family: sans-serif;
+  line-height: 1.15;
+  -webkit-text-size-adjust: 100%;
+  -webkit-tap-highlight-color: rgba(0,0,0,0)
+}
+
+body {
+  margin: 0;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  text-align: left;
+  background-color: #fff;
+}
+
+@media (min-width: $screen-md) {
+  html, body {
+    min-height: 100%;
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Merriweather Sans', sans-serif;
+    // @TODO: background: url(/assets/bg-a0714f6….gif);
+    color: #000;
+  }
+}
 
 .imimap-header {
   background: url('/assets/topbar-background.png'),#292929;
@@ -121,6 +188,33 @@ $screen-md: 768px;
   }
 }
 
+.imi-nav-right {
+  margin-left: 50%;
+}
+
+.float-right {
+  float: right !important;
+}
+
+.imi-nav-right>li>a {
+  text-decoration: none;
+}
+
+.imi-nav-right>li>a {
+  float: left;
+  display: inline;
+  vertical-align: top;
+  font-size: 12px;
+  font-weight: bold;
+  background-color: #77b900 !important;
+  color: #fff !important;
+  padding-bottom: 3px;
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-top: 3px;
+  font-weight: 100;
+}
+
 .navbar-dark .navbar-toggler {
   color: rgba(255,255,255,0.5);
   border-color: rgba(255,255,255,0.1);
@@ -134,5 +228,21 @@ $screen-md: 768px;
 
 .bg-dark.navbar-dark {
   background-color: transparent !important;
+}
+
+.imi-map-navlink {
+  color: #c0b9b9 !important;
+  text-decoration: none;
+}
+
+.im-nav-link {
+  color: #000;
+  text-shadow: none;
+  font-family: 'Josefin Sans', sans-serif;
+  text-transform: uppercase;
+  text-decoration: none;
+  font-size: 1rem;
+  float: right;
+  color: #6b6b6b;
 }
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,17 @@
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCog, faQuestionCircle, faSignOutAlt, faUser,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router/index';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).use(router).mount('#app');
+library.add(faQuestionCircle);
+library.add(faUser);
+library.add(faCog);
+library.add(faSignOutAlt);
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
