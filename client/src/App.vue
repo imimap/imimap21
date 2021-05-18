54bcff221531450e8f9b000088009449a1730f6a
Robin <robin.decker@student.htw-berlin.de>
2021-05-18 22:01:26 +0200

    #31 added department in table view

--- client/src/App.vue
+++ client/src/App.vue
@@ -25,7 +25,7 @@ html {
   font-family: sans-serif;
   line-height: 1.15;
   -webkit-text-size-adjust: 100%;
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0)
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
 }
 
 body {
