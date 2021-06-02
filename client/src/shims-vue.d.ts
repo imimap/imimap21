3ff4c01c05629acdfd848695aad3d498a9c0bd45
Robin <robin.decker@student.htw-berlin.de>
2021-06-02 10:37:29 +0200

    #32 added leadflet package

--- client/src/shims-vue.d.ts
+++ client/src/shims-vue.d.ts
@@ -1,8 +1,7 @@
 import { Route, Router } from 'vue-router';
+import type { DefineComponent } from 'vue';
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
@@ -13,4 +12,17 @@ declare module 'vue/types/vue' {
     $router: Router;
     $route: Route;
   }
+}
+
+declare module '@vue-leaflet/vue-leaflet' {
+  export const LMap: DefineComponent;
+  export const LIcon: DefineComponent;
+  export const LTileLayer: DefineComponent;
+  export const LMarker: DefineComponent;
+  export const LControlLayers: DefineComponent;
+  export const LTooltip: DefineComponent;
+  export const LPopup: DefineComponent;
+  export const LPolyline: DefineComponent;
+  export const LPolygon: DefineComponent;
+  export const LRectangle: DefineComponent;
 }
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,34 @@
 <template>
   <h2>Home View</h2>
+  <l-map
+    v-model="zoom"
+    v-model:zoom="zoom"
+    :center="[47.41322, -1.219482]"
+    @move="log('move')"
+  >
+  </l-map>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { LMap } from '@vue-leaflet/vue-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    LMap,
+  },
+  data() {
+    return {
+      zoom: 2,
+    };
+  },
+  method: {
+    log(a: string) {
+      console.log(a);
+    },
+  },
 });
 </script>
 
