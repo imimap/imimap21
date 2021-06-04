83b0c09470d9029ff0f83ce8f259306d5b9f1a3c
Luis Hankel <kaes3kuch3n@gmail.com>
2021-06-04 10:15:40 +0200

    Added bootstrap overrides

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,10 @@
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faCog, faQuestionCircle, faSignOutAlt, faUser,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCog, faQuestionCircle, faSignOutAlt, faUser, } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import locales from './locales/locales';
 import App from './App.vue';
 import router from './router';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 library.add(faQuestionCircle);
 library.add(faUser);
