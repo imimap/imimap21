33135467a9fb49172df114e22b378c25e1ba0c34
Jakob Panten <jakob.panten@idealo.de>
2021-06-14 18:48:47 +0200

    changes display of InternshipPart status

--- client/src/components/admin/UsersList.vue
+++ client/src/components/admin/UsersList.vue
@@ -85,16 +85,34 @@
                             </div>
                             <div class="col-lg-8 col-md-12 mb-2 status-internship-part">
                               <span class="fw-bold list-item-label">Status</span><br>
-                                  <span class="badge rounded-pill bg-success d-block mb-2">
-                                    Antrag vorhanden</span>
-                                  <span class="badge rounded-pill bg-success d-block mb-2">
-                                    Vertrag vorhanden</span>
-                                  <span class="badge rounded-pill bg-secondary d-block mb-2">
-                                    ECTS-Nachweis vorhanden
-                                  </span>
-                                  <span class="badge rounded-pill bg-secondary d-block mb-2">
-                                    Zertifikat vorhanden
-                                  </span>
+
+                              <ul class="list-group">
+                                <li class="list-group-item list-group-item-success">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                       height="16" fill="currentColor"
+                                       class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75
+                                    0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06
+                                    1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0
+                                    0-.01-1.05z"/>
+                                  </svg>
+                                  Antrag vorhanden</li>
+                                <li class="list-group-item list-group-item-success">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                       height="16" fill="currentColor"
+                                       class="bi bi-check-circle-fill" viewBox="0 0 16 16">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75
+                                    0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06
+                                    1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0
+                                    0-.01-1.05z"/>
+                                  </svg>
+                                  <i class="bi bi-check-circle-fill"></i>
+                                  Vertrag vorhanden</li>
+                                <li class="list-group-item list-group-item-light">
+                                  ECTS-Nachweis fehlt</li>
+                                <li class="list-group-item list-group-item-light">
+                                  Zertifikat fehlt</li>
+                              </ul>
                             </div>
                           </div>
                         </div>
