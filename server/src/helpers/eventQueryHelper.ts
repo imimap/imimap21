5506a6a489fe4b36843b4d073e691ddafcd9ae97
Linda <fernsel@htw-berlin.de>
2021-07-05 10:16:52 +0200

    refactors eventQueryHelper

--- server/src/helpers/eventQueryHelper.ts
+++ server/src/helpers/eventQueryHelper.ts
@@ -1,50 +1,45 @@
 import { Document } from "mongoose";
 import { IEvent } from "../models/eventModels/event";
 
-export const getRecentValueForPropSetByEvent = (propName: string, document: Document): any => {
-  let recentPropValue;
-  let i = -1; // counter starts with last element in events array
-
-  const events = document.get("events").filter((event: IEvent) => {
+function getValue(allEvents: IEvent[], propName: string) {
+  const events = allEvents.filter((event: IEvent) => {
     return event.changes;
   });
+
+  let recentPropValue;
+  let i = -1;
+
   while (!recentPropValue && i >= events.length * -1) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     recentPropValue = events.slice(i)[0].changes[propName];
     i--;
   }
   return recentPropValue;
+}
+
+export const getRecentValueForPropSetByEvent = (propName: string, document: Document): any => {
+  return getValue(document.get("events"), propName);
 };
 
 export const getRecentNotRejectedValueForPropSetByEvent = (
   propName: string,
   document: Document
 ): any => {
-  let recentPropValue;
-  let i = -1;
-
   const events = document.get("events").filter((event: IEvent) => {
-    return event.changes && event.accept !== false;
+    return event.accept !== false;
   });
-  while (!recentPropValue && i >= events.length * -1) {
-    recentPropValue = events.slice(i)[0].changes[propName];
-    i--;
-  }
-  return recentPropValue;
+
+  return getValue(events, propName);
 };
 
 export const getRecentAcceptedValueForPropSetByEvent = (
   propName: string,
   document: Document
 ): any => {
-  let recentPropValue;
-  let i = -1;
-
   const events = document.get("events").filter((event: IEvent) => {
-    return event.changes && event.accept === true;
+    return event.accept === true;
   });
-  while (!recentPropValue && i >= events.length * -1) {
-    recentPropValue = events.slice(i)[0].changes[propName];
-    i--;
-  }
-  return recentPropValue;
+
+  return getValue(events, propName);
 };
