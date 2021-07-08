6fdf7c7d2fbe1a74d309bae922a0d42e9d6468dd
Luis Hankel <kaes3kuch3n@gmail.com>
2021-07-08 13:10:53 +0200

    Fixed postponement requests endpoint

--- server/src/routes/internshipModule.ts
+++ server/src/routes/internshipModule.ts
@@ -23,17 +23,17 @@ internshipModuleRouter.get(
 );
 
 internshipModuleRouter.get(
+  "/postponements",
+  authMiddleware(true),
+  asyncHandler(listPostponementRequests)
+);
+
+internshipModuleRouter.get(
   "/:id",
   authMiddleware(),
   param("id").custom((id) => /[0-9a-f]{24}/.test(id) || id === "my"),
   validate,
   asyncHandler(findInternshipModule)
-);
-
-internshipModuleRouter.get(
-  "/postponements",
-  authMiddleware(true),
-  asyncHandler(listPostponementRequests)
 );
 
 export default internshipModuleRouter;
