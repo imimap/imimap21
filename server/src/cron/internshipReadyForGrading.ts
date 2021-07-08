670e8ff3d178d7814c0f9c6ddeb34133ca98b606
Linda <LiFaytheGoblin@users.noreply.github.com>
2021-07-08 13:27:05 +0200

    removes $

--- server/src/cron/internshipReadyForGrading.ts
+++ server/src/cron/internshipReadyForGrading.ts
@@ -4,8 +4,8 @@ import { PdfDocumentStatuses } from "../models/pdfDocument";
 export async function checkInternshipReadyForGrading(): Promise<void> {
   const internships = await Internship.find({
     status: InternshipStatuses.OVER,
-    "reportPdf.$.status": PdfDocumentStatuses.SUBMITTED,
-    "certificatePdf.$.status": PdfDocumentStatuses.SUBMITTED,
+    "reportPdf.status": PdfDocumentStatuses.SUBMITTED,
+    "certificatePdf.status": PdfDocumentStatuses.SUBMITTED,
   });
   for (const internship of internships) {
     await trySetReadyForGrading(internship);
