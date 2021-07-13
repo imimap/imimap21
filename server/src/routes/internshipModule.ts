60fdbcd735a9fe0f6ca583c6d34a7b1753c5ecce
Linda <fernsel@htw-berlin.de>
2021-07-13 13:32:06 +0200

    adds getter stubs for pdfs

--- server/src/routes/internshipModule.ts
+++ server/src/routes/internshipModule.ts
@@ -76,6 +76,24 @@ internshipModuleRouter.patch(
   asyncHandler(rejectReportPdf)
 );
  */
+/*
+internshipModuleRouter.get(
+  "/:id/pdf/report/",
+  authMiddleware(),
+  param("id").custom((id) => /[0-9a-f]{24}/.test(id)),
+  validate,
+  asyncHandler(getReportPdf)
+);
+ */
+/*
+internshipModuleRouter.get(
+  "/:id/pdf/complete/",
+  authMiddleware(),
+  param("id").custom((id) => /[0-9a-f]{24}/.test(id)),
+  validate,
+  asyncHandler(getCompleteDocumentsPdf)
+);
+ */
 
 /* Additional admin interface options */
 internshipModuleRouter.patch(
