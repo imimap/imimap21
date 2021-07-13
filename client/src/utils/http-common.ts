1d17c1f1f44d2c302862f273e80fdc917f3d1b25
Luis Hankel <kaes3kuch3n@gmail.com>
2021-07-13 18:47:42 +0200

    Added API host

--- client/src/utils/http-common.ts
+++ client/src/utils/http-common.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: `${process.env.API_HOST}/api`,
+  baseURL: 'http://imimap.f4.htw-berlin.de:9000/api',
   headers: {
     'Content-type': 'application/json',
   },
