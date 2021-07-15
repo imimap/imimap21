5f23d70b8c036822b42b4cb38bf9c5adc5595765
Jakob Panten <jakob.panten@idealo.de>
2021-07-15 15:48:40 +0200

    add endpoint to get student by Id for admin postponement-requests list

--- server/src/controllers/student.ts
+++ server/src/controllers/student.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user";
+import { Forbidden, NotFound } from "http-errors";
 
 export async function getStudents(req: Request, res: Response, next: NextFunction): Promise<void> {
   const options: any = {
@@ -10,4 +11,24 @@ export async function getStudents(req: Request, res: Response, next: NextFunctio
   if (req.params.id) options._id = req.params.id;
   const users = await User.find(options);
   res.json(users);
+}
+
+export async function getStudentById(
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> {
+  const user = await User.findOne({ emailAddress: req.user?.email }).lean().select("isAdmin");
+  if (!user) return next(new NotFound("User not found"));
+  if (!user.isAdmin) return next(new Forbidden("Only admins may get all student details."));
+
+  const studentId = req.params.id.toString();
+
+  if (user.isAdmin) {
+    const student = await User.findById(studentId).lean();
+    if (!student) return next(new NotFound("Student not found"));
+    res.json(student);
+  } else {
+    return next(new Forbidden("You are not allowed to fetch student details."));
+  }
 }
--- server/src/routes/student.ts
+++ server/src/routes/student.ts
@@ -4,15 +4,18 @@ import { Semester } from "../helpers/semesterHelper";
 import authMiddleware from "../authentication/middleware";
 import { validate } from "../helpers/validation";
 import * as asyncHandler from "express-async-handler";
-import { getStudents } from "../controllers/student";
+import { getStudentById, getStudents } from "../controllers/student";
 
 const studentRouter = Router();
 
+studentRouter.get("/", authMiddleware(true), validate, asyncHandler(getStudents));
+
 studentRouter.get(
-  "/",
-  authMiddleware(true),
+  "/:id",
+  authMiddleware(),
+  param("id").custom((id) => /[0-9a-f]{24}/.test(id)),
   validate,
-  asyncHandler(getStudents)
+  asyncHandler(getStudentById)
 );
 
 export default studentRouter;
