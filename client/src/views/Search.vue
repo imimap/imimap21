2e814cc9f70bed88470e4dfca4038531a6b7747a
Linda <fernsel@htw-berlin.de>
2022-02-04 15:25:59 +0100

    refactors search result page and tries to display previous search results

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -119,71 +119,16 @@
       </button>
     </div>
     <div id="search-results" class="search_results" v-if="!cardToggle">
-      <div class="container" style="max-width: 100vw;">
-        <p class="text-center p-1">
-          {{ $tc("search.results.resultCount", resultCount) }}
-        </p>
-        <!-- @TODO: %count% Ergebnisse aus vorherigen suchen implementieren-->
-        <p class="text-center p-1" v-if="false">
-          {{ $tc("search.results.previousResultCount", resultCount) }}
-        </p>
-        <table class="table table-striped table-sm table-borderless text-left">
-          <tbody>
-          <tr>
-            <td class="font-weight-bold">{{ $t("search.company") }}</td>
-            <td class="font-weight-bold">{{ $t("search.location") }}</td>
-            <td class="font-weight-bold">{{ $t("search.orientation") }}</td>
-            <td class="font-weight-bold"></td>
-          </tr>
-          <!-- Result Loop -->
-          <template  v-if="searchResults?.length > 0">
-            <template v-for="(searchResult) in searchResults"
-                      v-bind:key="searchResult._id">
-              <tr>
-                <td>{{ searchResult?.company?.companyName }}</td>
-                <td> {{ searchResult?.company?.address?.city }}</td>
-                <td> {{ searchResult?.operationalArea }}</td>
-                <td>
-                  <button class="btn btn-outline-htw-green float-right"
-                          data-bs-toggle="collapse"
-                          :data-bs-target="'#collapseResult' + searchResult._id"
-                          aria-expanded="false"
-                          :aria-controls="'#collapseResult' + searchResult._id">
-                    Details
-                  </button>
-                </td>
-              </tr>
-              <tr class="collapse" :id="'collapseResult' + searchResult._id">
-                <td colspan="7">
-                  <p class="pl-3">
-                    <strong>{{ $t("search.programmingLanguages") }}</strong>
-                    {{
-                      searchResult.programmingLanguages?.length > 0
-                        ? searchResult.programmingLanguages?.toString()
-                        :  'Keine Angabe'
-                    }}
-                  </p>
-                  <p class="pl-3">
-                    <strong>{{ $t("search.website") }}</strong>
-                    <a :href="searchResult?.company?.website" target="_blank">
-                      {{ searchResult?.company?.website }}
-                    </a>
-                  </p>
-                  <p class="pl-3">
-                    <strong>{{ $t("search.tasks") }}</strong>
-                    {{ searchResult?.tasks }}
-                  </p>
-                  <p class="pl-3">
-                    <strong>{{ $t("search.contact") }}</strong>
-                    {{ searchResult?.company?.emailAddress }}
-                  </p>
-                </td>
-              </tr>
-            </template>
-          </template>
-          </tbody>
-        </table>
-      </div>
+      <SearchResultList
+        :result-count="resultCount"
+        :search-results="searchResults"
+        result-count-text="search.results.resultCount">
+      </SearchResultList>
+      <SearchResultList
+        :result-count="previousResultCount"
+        :search-results="previousSearchResults"
+        result-count-text="search.results.previousResultCount">
+      </SearchResultList>
     </div>
     <div id="map-results">
       <Map v-if="cardToggle" :locations="locations"></Map>
@@ -198,10 +143,11 @@ import Map from '@/components/Map.vue';
 import http from '@/utils/http-common';
 import { Internship } from '@/store/types/Internship';
 import { MapLocation } from '@/store/types/MapLocation';
+import SearchResultList from '@/components/search/SearchResultList.vue';
 
 export default defineComponent({
   name: 'Search',
-  components: { Map },
+  components: { SearchResultList, Map },
   data() {
     return {
       // Available filters after query
@@ -211,6 +157,7 @@ export default defineComponent({
       availableLanguages: null,
       // Searchresults after query
       searchResults: [] as Internship[],
+      previousSearchResults: [] as Internship[],
       // Selected filters
       paymentFilter: null,
       countryFilter: null,
@@ -224,6 +171,9 @@ export default defineComponent({
   computed: {
     resultCount(): number {
       return this.searchResults.length;
+    },
+    previousResultCount(): number {
+      return this.previousSearchResults.length;
     },
     locations(): MapLocation[] | null {
       if (this.searchResults.length === 0) return null;
@@ -283,23 +233,39 @@ export default defineComponent({
     async searchRequest() {
       this.loadingState = true;
       try {
+        this.searchResults = await this.getSearchResults(false);
+        // this.searchResults = this.searchResults.filter((internship) =>
+        // typeof internship.company.address !== 'undefined');
+      } catch (e: any) {
+        await this.$store.dispatch('addNotification', {
+          text: e.message,
+          type: 'danger',
+        });
+      }
+      try {
+        this.previousSearchResults = await this.getSearchResults(true);
+      } catch (e: any) {
+        await this.$store.dispatch('addNotification', {
+          text: e.message,
+          type: 'danger',
+        });
+      }
+      this.loadingState = false;
+    },
+    async getSearchResults(seen: boolean) {
+      try {
         const res = await http.get('/internships', {
           params: {
             country: this.countryFilter,
             operationalArea: this.operationalAreaFilter,
             programmingLanguage: this.languageFilter,
             paymentType: this.paymentFilter,
-            seen: false,
+            seen,
           },
         });
-        this.searchResults = res.data;
-        this.searchResults = this.searchResults.filter((internship) => typeof internship.company.address !== 'undefined');
-        this.loadingState = false;
+        return res.data;
       } catch (err) {
-        await this.$store.dispatch('addNotification', {
-          text: `Fehler beim Suchen nach Praktika [ERROR: ${err.message}]`,
-          type: 'danger',
-        });
+        throw new Error(`Fehler beim Suchen nach Praktika [ERROR: ${err.message}]`);
       }
     },
   },
